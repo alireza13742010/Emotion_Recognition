36b2b809120435123bd3a10f18ef5447a2e174e7
AlireZ@20 <56340275+alireza13742010@users.noreply.github.com>
2024-10-19 14:57:05 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import AutoModelForSequenceClassification
+from lingua import Language, LanguageDetectorBuilder
+from easygoogletranslate import EasyGoogleTranslate
+from transformers import pipeline
+Emotion_detection("Enter your text here")
