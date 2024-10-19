df0299999e89f23cd5ac8cf4c2b421a83db1a94c
AlireZ@20 <56340275+alireza13742010@users.noreply.github.com>
2024-10-19 15:21:25 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from lingua import Language, LanguageDetectorBuilder
 from Emotion_Recognition import Emotion_detection
-from Translator import translaor
+from Translate import translaor
+from Language_Detection import language_detector
+from Lable_Prediction import lable_prediction
 from easygoogletranslate import EasyGoogleTranslate
 from transformers import pipeline
 import AutoModelForSequenceClassification
